bf127dc927befb2e7a58db4ad070848cf41934c2
Shahriyar <mdshahriyaralam552@gmail.com>
2021-08-07 00:45:18 +0600

    Change version and update install requires

--- flask_maker/__init__.py
+++ flask_maker/__init__.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 from .cli import cli
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 VersionInfo = namedtuple("VersionInfo", "major minor macro release")
 
-version_info = VersionInfo(2, 0, 0, "stable")
+version_info = VersionInfo(2, 0, 1, "stable")
